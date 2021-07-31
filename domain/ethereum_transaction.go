0111bb06b64168aebc28e4d224844366791b111e
Dmitry Ivanov <me@d-ops.pro>
2021-07-31 21:27:38 +0300

    Fix ethereum input generation
    Fixed values converting from int to []byte,
     which is required for the twallet sign
     operation. This values should be in HEX format.

--- domain/ethereum_transaction.go
+++ domain/ethereum_transaction.go
@@ -1,7 +1,8 @@
 package domain
 
 import (
-	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"unsafe"
 
 	"github.com/default23/crypto-api/domain/protos/ethereum"
@@ -20,16 +21,16 @@ type EthereumTransaction struct {
 
 func (tx EthereumTransaction) SignInput(w Wallet) ([]byte, error) {
 	input := ethereum.SigningInput{
-		ChainId:    toByteArray(tx.ChainID),
-		Nonce:      toByteArray(tx.Nonce),
-		GasPrice:   toByteArray(tx.GasPrice),
-		GasLimit:   toByteArray(tx.GasLimit),
+		ChainId:    toHex(tx.ChainID),
+		Nonce:      toHex(tx.Nonce),
+		GasPrice:   toHex(tx.GasPrice),
+		GasLimit:   toHex(tx.GasLimit),
 		ToAddress:  tx.ToAddress,
 		PrivateKey: twutil.TWDataGoBytes(unsafe.Pointer(w.GetPrivateKey())),
 		Transaction: &ethereum.Transaction{
 			TransactionOneof: &ethereum.Transaction_Transfer_{
 				Transfer: &ethereum.Transaction_Transfer{
-					Amount: toByteArray(tx.Value),
+					Amount: toHex(tx.Value),
 				},
 			},
 		},
@@ -53,9 +54,8 @@ func (tx *EthereumTransaction) GetSignedOutput(signed []byte) ([]byte, error) {
 	return output.Encoded, nil
 }
 
-func toByteArray(i int) []byte {
-	out := make([]byte, 32)
-	binary.BigEndian.PutUint32(out, uint32(i))
-
-	return out
+func toHex(i int) []byte {
+	// skip errors, because it will be always valid hex string
+	h, _ := hex.DecodeString(fmt.Sprintf("%x", i))
+	return h
 }
